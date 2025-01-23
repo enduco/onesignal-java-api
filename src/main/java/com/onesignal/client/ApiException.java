ff2c13e6f8fa68ef20d10dbada5c037748f2e335
Thomas Bruckmann <thomas.bruckmann@enduco.app>
2025-01-23 10:25:46 +0100

    fix: response headers toString

--- src/main/java/com/onesignal/client/ApiException.java
+++ src/main/java/com/onesignal/client/ApiException.java
@@ -13,6 +13,7 @@
 
 package com.onesignal.client;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
 
@@ -25,7 +26,7 @@ import javax.ws.rs.core.GenericType;
 @javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-08-01T23:27:12.168Z[Etc/UTC]")
 public class ApiException extends Exception {
     private int code = 0;
-    private Map<String, List<String>> responseHeaders = null;
+    private Map<String, List<String>> responseHeaders = new HashMap<>();
     private String responseBody = null;
  
     /**
